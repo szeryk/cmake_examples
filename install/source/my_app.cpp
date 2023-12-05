e337dbc9192a6decdca5a3edde382ce2aa7ab77b
Eryk Szyfner <eryk.szyfner@gmail.com>
2023-12-05 14:22:18 +0100

    Add install example

---
+++ install/source/my_app.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include "my_lib.h"
+
+int main() {
+    std::cout << "MyApp is working" << std::endl;
+    lib_fun();
+}
---
+++ install/source/my_lib/my_lib.cpp
@@ -0,0 +1,7 @@
+#include "my_lib.h"
+
+#include <iostream>
+
+void lib_fun() {
+    std::cout << "Using MyLib" << std::endl;
+}
