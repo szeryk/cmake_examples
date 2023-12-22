0e253728623464fc020a32fc6fc01fd228ae52a1
Eryk Szyfner <eryk.szyfner@gmail.com>
2023-12-22 16:47:57 +0100

    Add system introspection example

---
+++ system_introspection/source/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+// #include "host_info.h"
+
+int main() {
+    std::cout << "hello world!\n";
+    // #ifdef HAVE_LOG
+    //     std::cout << "standard log() function available!\n";
+    // #endif
+
+    // std::cout << "Machine: " HOST_NAME << " has OS: " OS_NAME << std::endl;
+}
